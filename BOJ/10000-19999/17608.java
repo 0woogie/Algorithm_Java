e2f10dc9220263c4e840f93fa071db3ab804f9e6
0woogie <80441481+0woogie@users.noreply.github.com>
2023-07-11 14:28:49 +0900

    Create 17608.java

---
+++ BOJ/10000-19999/17608.java
@@ -0,0 +1,25 @@
+#막대기
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class B17608 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		Stack<Integer> stack = new Stack<>();
+		for(int i=0; i<n; i++) {
+			stack.push(sc.nextInt());
+		}
+		int maxNum = 0;
+		int cnt = 0;
+		while(!stack.isEmpty()) {
+			int now = stack.pop();
+			if(now > maxNum) {
+				maxNum = now;
+				cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
