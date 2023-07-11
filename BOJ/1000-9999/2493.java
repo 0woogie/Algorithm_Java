5e4e84d2370430ed3334b717bdb37c14f9da82e6
0woogie <80441481+0woogie@users.noreply.github.com>
2023-07-11 17:33:48 +0900

    Create 2493.java

---
+++ BOJ/1000-9999/2493.java
@@ -0,0 +1,43 @@
+#탑
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+class Node {
+	int value;
+	int index;
+}
+
+public class B2493 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Stack<Node> stack1 = new Stack<>();
+		for(int i=0; i<n; i++) {
+			Node node = new Node();
+			node.index = i+1;
+			node.value = Integer.parseInt(st.nextToken());
+			stack1.push(node);
+		}
+		Stack<Node> stack2 = new Stack<>();
+		int[] result = new int[n+1];
+		while(!stack1.isEmpty()) {
+			Node a = stack1.pop();
+			while(!stack2.isEmpty()) {
+				Node b = stack2.pop();
+				if(b.value<a.value) {
+					result[b.index] = a.index;
+				} else {
+					stack2.push(b);
+					break;
+				}
+			}
+			stack2.push(a);
+		}
+		for(int i=1; i<n+1; i++) System.out.print(result[i]+" ");
+	}
+}
