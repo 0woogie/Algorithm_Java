7b810a57e0c9405101088f408c376973c4703b8f
0woogie <80441481+0woogie@users.noreply.github.com>
2023-07-17 10:48:29 +0900

    Update 5215.java

--- SWEA/5215.java
+++ SWEA/5215.java
@@ -5,6 +5,48 @@ import java.util.Scanner;
 public class S5215 {
 
 	static int result;
+	static int N, L;
+	static int[][] arr;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for(int test_case = 1; test_case <= T; test_case++) {
+			N = sc.nextInt(); //재료의 수
+			L = sc.nextInt(); //제한 칼로리
+			arr = new int[N][2];
+			for(int i=0; i<N; i++) {
+				arr[i][0] = sc.nextInt(); //재료 i의 점수
+				arr[i][1] = sc.nextInt(); //재료 i의 칼로리
+			}
+			result = 0;
+			dfs(0, 0, 0); //n, score, cal
+			System.out.println("#"+test_case+" " + result);
+		}
+	}
+
+	private static void dfs(int n, int score, int cal) {
+		if(cal>L) return;
+		if(n==N) {
+			if(score>result) result = score;
+			return;
+		}
+		
+		//선택하는 경우
+		dfs(n+1, score+arr[n][0], cal+arr[n][1]);
+		//선택하지 않는 경우
+		dfs(n+1, score, cal);
+	}
+
+}
+
+/*
+//조합을 풀 때는 위의 방식(이진 트리)으로 구현하는 것이 더 적합함. 아래의 방식은 N이 대략 15를 넘어가면 과부하가 온다.
+import java.util.Scanner;
+
+public class S5215 {
+
+	static int result;
 	static int N, M;
 	static int L;
 	static int[][] arr;
@@ -52,3 +94,4 @@ public class S5215 {
 	}
 
 }
+*/
