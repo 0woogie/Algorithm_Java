aa3b74c4cce5697e5c5ca9faafe885e21bfe8baf
0woogie <80441481+0woogie@users.noreply.github.com>
2023-07-19 15:16:04 +0900

    Create 2667.java

---
+++ BOJ/1000-9999/2667.java
@@ -0,0 +1,58 @@
+#단지번호붙이기
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class B2667 {
+	
+	static int n;
+	static int cnt;
+	static int[][] graph;
+	static boolean[][] visited;
+	static List<Integer> result;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		graph = new int[n][n];
+		visited = new boolean[n][n];
+		result = new ArrayList<>();
+		for(int i=0; i<n; i++) {
+			String s = sc.next();
+			for(int j=0; j<n; j++) {
+				graph[i][j] = s.charAt(j)-'0';
+			}
+		}
+		int danji = 0;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(graph[i][j]!=0 && !visited[i][j]) {
+					cnt = 0;
+					DFS(i,j);
+					result.add(cnt);
+					danji++;
+				}
+			}
+		}
+		Collections.sort(result);
+		System.out.println(danji);
+		for(int i=0; i<danji; i++) {
+			System.out.println(result.get(i));
+		}
+	}
+	
+	private static void DFS(int x, int y) {
+		if(x<0 || x>=n || y<0 || y>=n) return;
+		if(graph[x][y]==0 || visited[x][y]) return;
+		visited[x][y] = true;
+		cnt++;
+		DFS(x+1,y);
+		DFS(x,y+1);
+		DFS(x-1,y);
+		DFS(x,y-1);
+	}
+}
+
+//dx, dy 배열을 이용한 방식으로도 구현할 수 있음
