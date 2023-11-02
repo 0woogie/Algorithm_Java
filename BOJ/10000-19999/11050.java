237563c33193ef9122d63cb369f78a90844952d8
0woogie <80441481+0woogie@users.noreply.github.com>
2023-11-02 16:56:49 +0900

    Create 11050.java

---
+++ BOJ/10000-19999/11050.java
@@ -0,0 +1,26 @@
+#이항 계수 1
+
+import java.util.Scanner;
+
+public class B11050 {
+
+	static int[][] dp;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		dp = new int[N+1][K+1]; //dp[n][k] = nCk 값
+		System.out.println(Combi(N, K));
+	}
+	
+	static int Combi(int n, int k) {
+		if(k==0 || n==k)
+			return 1;
+		if(dp[n][k]!=0)
+			return dp[n][k];
+		return dp[n][k] = Combi(n-1, k-1) + Combi(n-1, k); //조합의 성질(파스칼의 삼각형)
+	}
+
+}
