f15440f17ca7f3a82a5047e1dfc1f11420c606da
0woogie <80441481+0woogie@users.noreply.github.com>
2023-11-17 17:26:17 +0900

    Create 5568.java

---
+++ BOJ/1000-9999/5568.java
@@ -0,0 +1,40 @@
+#카드 놓기
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class B5568 {
+	
+	static HashSet<String> set = new HashSet<>();
+	static String[] cards;
+	static boolean[] visited;
+	static int n, k;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		k = sc.nextInt();
+		cards = new String[n];
+		visited = new boolean[n];
+		for(int i=0; i<n; i++) {
+			cards[i] = sc.next();
+		}
+		DFS(0, ""); //count
+		System.out.println(set.size());
+	}
+	
+	static void DFS(int count, String result) {
+		if(count==k) {
+			set.add(result);
+		}
+		for(int i=0; i<n; i++) {
+			if(!visited[i]) {
+				visited[i] = true;
+				DFS(count+1, result+cards[i]);
+				visited[i] = false;
+			}
+		}
+	}
+}
+
+//백트래킹 + HashSet 사용
