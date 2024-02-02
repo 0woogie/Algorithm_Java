3780544fc884d67492cf8ad92b98f2a4104fd34d
0woogie <80441481+0woogie@users.noreply.github.com>
2024-02-03 01:42:51 +0900

    Create 바이러스.java
    구현/10분

---
+++ SOFT/바이러스.java
@@ -0,0 +1,19 @@
+#바이러스
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long K = Integer.parseInt(st.nextToken());
+        long P = Integer.parseInt(st.nextToken());
+        long N = Integer.parseInt(st.nextToken());
+        for(int i=0; i<N; i++) {
+            K *= P;
+            K %= 1000000007;
+        }
+        System.out.println(K);
+    }
+}
