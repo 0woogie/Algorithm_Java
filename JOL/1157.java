8d3728cc6c1144c937fca926722f25f997d1b3de
0woogie <80441481+0woogie@users.noreply.github.com>
2024-03-28 16:00:04 +0900

    Update 1157.java

--- JOL/1157.java
+++ JOL/1157.java
@@ -3,12 +3,12 @@
 import java.util.Scanner;
 
 public class J1157 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i=0; i<n; i++)
-        	arr[i] = sc.nextInt();
+            arr[i] = sc.nextInt();
         for(int i=0; i<n-1; i++) {
             for(int j=0; j<n-i-1; j++) {
                 if(arr[j]>arr[j+1]){
@@ -20,5 +20,5 @@ public class J1157 {
             for(int k=0; k<n; k++) System.out.print(arr[k]+" ");
             System.out.println();
         }
-	}
+    }
 }
