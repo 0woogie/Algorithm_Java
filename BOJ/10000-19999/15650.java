552928a41c7df4ef1683406c35eca50f385c4ae9
0woogie <80441481+0woogie@users.noreply.github.com>
2024-04-07 15:31:20 +0900

    Update 15650.java

--- BOJ/10000-19999/15650.java
+++ BOJ/10000-19999/15650.java
@@ -3,15 +3,14 @@
 import java.util.Scanner;
 
 public class B15650 {
+	
 	public static int[] result;
-	public static boolean[] visited;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
 		int m = sc.nextInt();
 		result = new int[m];
-		visited = new boolean[n];
 		dfs(n, m, 0, 0);
 	}
 	
@@ -23,15 +22,10 @@ public class B15650 {
 			return;
 		}
 		for(int i=start; i<n; i++) {
-			if(!visited[i]) {
-				visited[i] = true;
-				result[depth] = i+1;
-				dfs(n, m, depth+1, i+1);
-				visited[i] = false;
-			}
+			result[depth] = i+1;
+			dfs(n, m, depth+1, i+1);
 		}
 	}
 }
 
 //순열 문제(15649번)에서 달라진 부분은 dfs를 재귀 호출할 때마다 다음 시작 지점(변수 start)을 넘겨주는 것 밖에 없음
-//visited 배열로 방문 처리 할 필요 없음 (코드 수정 필요)
