dada420a1909ecee2bc475e7588135abb421fb09
0woogie <80441481+0woogie@users.noreply.github.com>
2024-04-09 00:26:28 +0900

    Update 2839.java

--- BOJ/1000-9999/2839.java
+++ BOJ/1000-9999/2839.java
@@ -22,3 +22,32 @@ public class B2839 {
 }
 
 //BOJ-14916번과 유사한 문제
+/*
+[다른 풀이 - DP]
+public class B2839 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		if(N==3 || N==5)
+			System.out.println(1);
+		else if(N==4)
+			System.out.println(-1);
+		else {
+			int[] D = new int[N+1];
+			Arrays.fill(D, Integer.MAX_VALUE);
+			D[3] = 1;
+			D[5] = 1;
+			for(int i=6; i<=N; i++) {
+				if(D[i-5]!=Integer.MAX_VALUE)
+					D[i] = D[i-5] + 1;
+				if(D[i-3]!=Integer.MAX_VALUE)
+					D[i] = Math.min(D[i], D[i-3]+1);
+			}
+			if(D[N]==Integer.MAX_VALUE)
+				System.out.println(-1);
+			else
+				System.out.println(D[N]);
+		}
+	}
+}
+*/
