bd0771d43b8e6e42177b36da8c1c92de4518b667
0woogie <80441481+0woogie@users.noreply.github.com>
2024-04-27 17:00:58 +0900

    Create 올바른괄호.java

---
+++ PGS/올바른괄호.java
@@ -0,0 +1,22 @@
+#올바른 괄호
+
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(c=='(') {
+                stack.push(c);
+            } else {
+                if(!stack.isEmpty()) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty() ? true : false;
+    }
+}
